02dce777c355aadf3922203a69fb0542797d0cff
Michel Lundell <michel@moose.se>
2016-09-02 09:33:36 +0200

    Initial Commit

---
+++ TestProjekt/Document.h
@@ -0,0 +1,12 @@
+//
+//  Document.h
+//  TestProjekt
+//
+//  Created by Michel Lundell on 02/09/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+@end
---
+++ TestProjekt/Document.m
@@ -0,0 +1,34 @@
+//
+//  Document.m
+//  TestProjekt
+//
+//  Created by Michel Lundell on 02/09/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import "Document.h"
+
+@interface Document ()
+
+@end
+
+@implementation Document
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
++ (BOOL)autosavesInPlace {
+    return YES;
+}
+
+- (void)makeWindowControllers {
+    // Override to return the Storyboard file name of the document.
+    [self addWindowController:[[NSStoryboard storyboardWithName:@"Main" bundle:nil] instantiateControllerWithIdentifier:@"Document Window Controller"]];
+}
+
+@end
